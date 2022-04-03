3c9c4128103951b197fcf572337c5cd3b489c95d
Paul Josserand <paul.josserand.rdl@gmail.com>
2022-04-03 10:53:45 +0200

    Comment Rect

--- Assets/LevelSelector.cs
+++ Assets/LevelSelector.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class LevelSelector : MonoBehaviour
 {
     private int _numberOfLevels;
-    private Rect 
+    //private Rect 
     // Start is called before the first frame update
     void Start()
     {
