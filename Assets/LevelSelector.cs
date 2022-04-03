af2f6f5bac012c4354899bf20e4a447b9a8d52f6
Paul Josserand <paul.josserand.rdl@gmail.com>
2022-04-03 10:53:19 +0200

    Level Selector
    Start Creating Level Selector

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelSelector : MonoBehaviour
+{
+    private int _numberOfLevels;
+    private Rect 
+    // Start is called before the first frame update
+    void Start()
+    {
+        _numberOfLevels = SceneManager.sceneCountInBuildSettings - 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
